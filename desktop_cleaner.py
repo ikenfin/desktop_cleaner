1d91f49e13dde62167572b2591a8bb61d9a9a4e5
ikenfin <ikenfin@gmail.com>
2015-06-02 08:58:03 +0300

    Option for excluding symlinks

--- desktop_cleaner.py
+++ desktop_cleaner.py
@@ -1,13 +1,19 @@
 #/usb/bin/python
+# -*- coding: utf-8 -*-
 
 import os, shutil, datetime
 
 # simple script for cleaning desktop
 
+# if true - prevent move symlinks
+exclude_symlinks = True
+
 # dir to clean
 dir_name = '/home/ken/Desktop/'
+
 # dir to store desktop items
 out_dir_name = 'desktop_vn'
+
 # exclude from cleaning
 white_list = [
 	'home',
@@ -23,6 +29,7 @@ to_dir = dir_name + out_dir_name + '/' + datetime.date.today().strftime('%Y-%m-%
 if os.path.exists(to_dir) == False :
 	os.makedirs(to_dir)
 
-for filename in from_dir : 
-	if(filename not in white_list) :
-		shutil.move(dir_name + filename, to_dir)
+for filename in from_dir :
+    if(filename not in white_list) :
+        if(not(exclude_symlinks) or not(os.path.islink(dir_name + filename))) :
+            shutil.move(dir_name + filename, to_dir)
